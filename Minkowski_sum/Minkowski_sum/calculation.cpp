b6cf4817d45450d82ce10b35d057362362df30f9
Dupeljan Linux <dupeljan@gmail.com>
2018-10-09 15:02:59 +0300

    Working solution

--- Minkowski_sum/Minkowski_sum/calculation.cpp
+++ Minkowski_sum/Minkowski_sum/calculation.cpp
@@ -15,17 +15,24 @@ Calculation::Calculation()
         obstacle.insert(make_pair(i,Lcorn));
      }
 
+    /*
+    for ( int i = Lcorn; i <= Rcorn; i++)
+        for ( int j = Lcorn; j <= Rcorn; j++)
+            obstacle.insert(make_pair(i,j));
+    */
     const double alpha = PI / 6;
     int centre = (Lcorn + Rcorn) / 2;
     set< pair < int , int > > rot_obstacle;
     // Rotate points
     std::cout << cos(alpha) << '|' << sin(alpha);
     for (  set< pair < int , int > >::iterator it = obstacle.begin(); it != obstacle.end(); it++ ){
-            int x = ( it->first + centre ) * cos(alpha) - ( it->second + centre ) * sin(alpha);
-            int y = ( it->first + centre ) * sin(alpha) + ( it->second + centre ) * cos(alpha);
+            int x = ( it->first - centre ) * cos(alpha) - ( it->second - centre ) * sin(alpha) +0.5;
+            int y = ( it->first - centre ) * sin(alpha) + ( it->second - centre ) * cos(alpha) +0.5;
+            x += centre;
+            y += centre;
             rot_obstacle.insert(make_pair(x  ,y ));
     }
-    obstacle = rot_obstacle;
+   obstacle = rot_obstacle;
 
     const int diam(41); // Robot const
     //Calculate cicle points
@@ -42,5 +49,12 @@ Calculation::Calculation()
         for( set< pair <int,int> >::iterator  j = robot.begin(); j != robot.end(); j++)
             result.insert(make_pair( i->first + j->first , i->second + j->second   ));
 
-
+    const int shift = 100;
+    set< pair < int , int > > shifted_robot;
+    for( set< pair <int,int> >::iterator  it = robot.begin(); it != robot.end(); it++){
+        int x = shift + it->first;
+        int y = shift + it->second;
+        shifted_robot.insert(make_pair(x,y));
+    }
+    robot = shifted_robot;
 }
